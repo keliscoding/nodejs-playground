54df78e90ef25eecf038f563f674521748fb2a56
Kelly Castelo <kawokkun@gmail.com>
2022-09-16 13:02:16 -0300

    trabalhando com parametros

--- NestJs-do-Zero/devtraining/src/courses/courses.controller.ts
+++ NestJs-do-Zero/devtraining/src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get('hello')
   sayHello(): string {
     return 'Hello!';
+  }
+
+  @Get('sayMyName/:name')
+  sayMyName(@Param('name') name: string): string {
+    return `your name is ${name}`;
   }
 }
