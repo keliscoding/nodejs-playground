7e0266667cd5f380bfa93a8133697ff84f450ae4
Kelly Castelo <kawokkun@gmail.com>
2022-09-18 11:12:51 -0300

    validando os dados recebidos pelo app

--- NestJs-do-Zero/devtraining/src/courses/dto/create-course.dto--no-spec.ts
+++ NestJs-do-Zero/devtraining/src/courses/dto/create-course.dto--no-spec.ts
@@ -1,6 +1,11 @@
+import { IsString } from 'class-validator';
+
 //nest g class courses/dto/create-course.dto --no-spec
 export class CreateCourseDto {
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly description: string;
+  @IsString({ each: true })
   readonly tags: string[];
 }
--- NestJs-do-Zero/devtraining/src/main.ts
+++ NestJs-do-Zero/devtraining/src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //precisa usar o class-validator e o class-transformer
+  app.useGlobalPipes(new ValidationPipe()); //serve para validar requisições
   await app.listen(3000);
 }
 bootstrap();
