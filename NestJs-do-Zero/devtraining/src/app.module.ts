4bd2cfdf3ff96d70fa7f8b24badeead859764636
Kelly Castelo <kawokkun@gmail.com>
2022-09-19 07:42:06 -0300

    criando a entidade course

--- NestJs-do-Zero/devtraining/src/app.module.ts
+++ NestJs-do-Zero/devtraining/src/app.module.ts
@@ -3,9 +3,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'devtraining',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- NestJs-do-Zero/devtraining/src/courses/courses.module.ts
+++ NestJs-do-Zero/devtraining/src/courses/courses.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { Course } from './entities/course.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])], //aqui ficam as entidades mapeadas do modulo
   controllers: [CoursesController],
   providers: [CoursesService],
 })
