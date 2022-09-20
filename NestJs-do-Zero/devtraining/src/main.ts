a1faa39f27f0e87843889af7922f40a0c123ac6f
Zam0k <kawokkun@gmail.com>
2022-09-20 15:44:44 -0300

    exemplo de teste e2e p1

--- NestJs-do-Zero/devtraining/src/main.ts
+++ NestJs-do-Zero/devtraining/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api');
   //precisa usar o class-validator e o class-transformer
   app.useGlobalPipes(
     new ValidationPipe({
--- NestJs-do-Zero/devtraining/test/courses/courses.e2e-spec.ts
+++ NestJs-do-Zero/devtraining/test/courses/courses.e2e-spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { CoursesModule } from '../../src/courses/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateCourseDto } from '../../src/courses/dto/create-course.dto--no-spec';
 
 describe('Courses: /courses', () => {
   let app: INestApplication;
+
+  const course: CreateCourseDto = {
+    name: 'Nestjs com Typeorm',
+    description: 'Criando apis restful com nestjs',
+    tags: ['nestjs', 'typeorm', 'nodejs', 'typescript'],
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,20 +26,33 @@ describe('Courses: /courses', () => {
           password: 'postgres',
           database: 'devtraining-tests',
           autoLoadEntities: true,
-          synchronize: true, 
+          synchronize: true,
         }),
       ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
   afterAll(async () => {
-    await app.close;
+    await app.close();
   });
 
-  it.todo('Create POST /courses');
+  it('Create POST /courses', () => {
+    return request(app.getHttpServer())
+      .post('/courses')
+      .send(course)
+      .expect(HttpStatus.CREATED);
+  });
+
   it.todo('Create GET /courses');
   it.todo('Create GET /courses/id');
   it.todo('Create PATCH /courses/id');
