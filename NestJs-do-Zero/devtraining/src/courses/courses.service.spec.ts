fd0d75c2ccc4df4cbeac9a0f5c9ffd573043eb83
Zam0k <kawokkun@gmail.com>
2022-09-20 13:13:48 -0300

    exemplo de teste unitario - courses service - p2

--- NestJs-do-Zero/devtraining/src/courses/courses.service.spec.ts
+++ NestJs-do-Zero/devtraining/src/courses/courses.service.spec.ts
@@ -1,27 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CoursesService } from './courses.service';
 import { Course } from './entities/course.entity';
 import { Tag } from './entities/tag.entity';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+});
+
 describe('CoursesService', () => {
   let service: CoursesService;
+  let coursesRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoursesService,
         { provide: Connection, useValue: {} },
-        { provide: getRepositoryToken(Course), useValue: {} },
-        { provide: getRepositoryToken(Tag), useValue: {} },
+        {
+          provide: getRepositoryToken(Course),
+          useValue: createMockRepository(),
+        },
+        { provide: getRepositoryToken(Tag), useValue: createMockRepository() },
       ],
     }).compile();
 
     service = module.get<CoursesService>(CoursesService);
+    coursesRepository = module.get<MockRepository>(getRepositoryToken(Course));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('find One', () => {
+    describe('buscar o curso pelo ID', () => {
+      it('deve retornar o objeto course', async () => {
+        const courseId = '1';
+        const expectCourse = {};
+
+        coursesRepository.findOne.mockReturnValue(expectCourse);
+
+        const course = await service.findOne(courseId);
+
+        expect(course).toEqual(expectCourse);
+      });
+
+      it('deve retornar NotFoundException', () => {
+        expect(service).toBeDefined();
+      });
+    });
   });
 });
