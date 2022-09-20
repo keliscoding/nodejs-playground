db52f7c4c40bd5a9f273f2523977f8c16c2183a7
Zam0k <kawokkun@gmail.com>
2022-09-20 13:54:02 -0300

    exemplo de teste e2e configurando banco de dados

--- NestJs-do-Zero/devtraining/test/courses/courses.e2e-spec.ts
+++ NestJs-do-Zero/devtraining/test/courses/courses.e2e-spec.ts
@@ -2,13 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { CoursesModule } from '../../src/courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('Courses: /courses', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [
+        CoursesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'postgres',
+          database: 'devtraining-tests',
+          autoLoadEntities: true,
+          synchronize: true, 
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -16,7 +29,7 @@ describe('Courses: /courses', () => {
   });
 
   afterAll(async () => {
-    app.close;
+    await app.close;
   });
 
   it.todo('Create POST /courses');
