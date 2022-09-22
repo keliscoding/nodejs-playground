673799b4b59887a5e61232af82f52fa0c10ce295
zam0k <kawokkun@gmail.com>
2022-09-22 11:15:23 -0300

    prisma client errors

--- NestJs-do-Zero/prisma-api/src/common/errors/interceptors/database.interceptor.ts
+++ NestJs-do-Zero/prisma-api/src/common/errors/interceptors/database.interceptor.ts
@@ -5,12 +5,16 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
+import { isPrimaError } from '../utils/is-prisma-error.utils';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
+        if (isPrimaError(error)) {
+        }
+
         throw error;
       }),
     );
