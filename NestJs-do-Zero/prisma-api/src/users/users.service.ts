d31bed71090c21dd421008d30274db26b3b27c4a
zam0k <kawokkun@gmail.com>
2022-09-22 09:34:35 -0300

    Users Repository

--- NestJs-do-Zero/prisma-api/src/users/users.service.ts
+++ NestJs-do-Zero/prisma-api/src/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(@Inject()) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
