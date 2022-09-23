7044c00c35570495cf1c5098d8469b74ee7d0691
zam0k <kawokkun@gmail.com>
2022-09-23 11:32:23 -0300

    ajustando as api responses na documentação

--- NestJs-do-Zero/prisma-api/src/users/users.controller.ts
+++ NestJs-do-Zero/prisma-api/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiForbiddenResponse, ApiResponse } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
@@ -15,27 +16,33 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiResponse({ status: 409, description: 'Conflito de email' })
+  @ApiForbiddenResponse({ description: 'Acesso negado' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiForbiddenResponse({ description: 'Acesso negado' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiForbiddenResponse({ description: 'Acesso negado' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiForbiddenResponse({ description: 'Acesso negado' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiForbiddenResponse({ description: 'Acesso negado' })
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
