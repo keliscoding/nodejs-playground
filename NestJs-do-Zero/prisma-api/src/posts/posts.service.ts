e89cadba21e62e9c0efa6ff3e8286f0db9662228
zam0k <kawokkun@gmail.com>
2022-09-23 08:38:50 -0300

    post controller e service

--- NestJs-do-Zero/prisma-api/src/posts/posts.service.ts
+++ NestJs-do-Zero/prisma-api/src/posts/posts.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundError } from '../common/errors/types/NotFoundError';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsRepository } from './repositories/posts.repository';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly repository: PostsRepository) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.repository.create(createPostDto);
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    return this.repository.findOne(id).catch(err => {
+      throw new NotFoundError(`Post ${id} not found`);
+    });
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    return this.repository.update(id, updatePostDto).catch(err => {
+      throw new NotFoundError(`Post ${id} not found`);
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number) {
+    return this.repository.remove(id).catch(err => {
+      throw new NotFoundError(`Post ${id} not found`);
+    });
   }
 }
